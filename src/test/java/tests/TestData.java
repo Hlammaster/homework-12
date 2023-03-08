aabfb2e78f1167644c5c5243563695d3d67c342c
Johnny Firo <hlammaster@mail.ru>
2023-03-08 16:29:18 +0300

    added javafakerfaker

---
+++ src/test/java/tests/TestData.java
@@ -0,0 +1,20 @@
+package tests;
+
+public class TestData {
+
+    public static String[] months = {"January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"};
+
+    public static String[] subjects = {"Accounting", "Maths", "Arts", "English", "Physics", "Chemistry",
+            "Computer Science", "Economics", "Social Studies", "History", "Civics", "Commerce", "Hindi", "Biology"};
+    public static String[] hobbiess = {"Reading", "Sports", "Music"};
+
+    public static String[] cities = {"Delhi", "Gurgaon", "Noida"};
+
+    public static String[] states = {"NCR", "Uttar Pradesh", "Haryana", "Rajasthan"};
+
+
+    public static String[] gender = {"Make", "Female", "Other"};
+
+
+}
--- src/test/java/tests/TestPracticeForm.java
+++ src/test/java/tests/TestPracticeForm.java
@@ -1,30 +1,49 @@
 package tests;
 
+import com.github.javafaker.Faker;
 import org.junit.jupiter.api.Test;
 import pages.RegistrationPage;
+
+import static tests.TestData.*;
+import static utils.RandomUtils.getRandomItemFromArray;
 
 
 public class TestPracticeForm extends TestBase {
     RegistrationPage registrationPage = new RegistrationPage();
-
+    Faker faker = new Faker();
 
     @Test
     void fillFormTest() {
+        String firstName = faker.name().firstName(),
+                lastName = faker.name().lastName(),
+                userEmail = faker.internet().emailAddress(),
+                gender = getRandomItemFromArray(gender),
+                userNumber = 8 + faker.phoneNumber().subscriberNumber(9),
+                dayOfBirth = String.format("%02d", faker.number().numberBetween(1, 28)),
+                monthOfBirth = getRandomItemFromArray(months),
+                yearOfBirth = String.valueOf(faker.number().numberBetween(1950, 2005)),
+                subject = getRandomItemFromArray(subjects),
+                hobbies = getRandomItemFromArray(hobbiess),
+                pictureName = "wallpaperflare.com_wallpaper (13).jpg",
+                currentAddress = faker.address().streetAddress(),
+                state = getRandomItemFromArray(states),
+                cities = getRandomItemFromArray(cities);
+
 
         registrationPage.openPage()
                 .removebannner()
-                .setFirstName("Sirius")
-                .setLastName("Black")
-                .setEmail("cat@mail.ru")
-                .setGender("Male")
-                .userNumberInput("1234567890")
-                .setBirthDate("30", "July", "2020")
-                .subjectsInput("Chemistry")
-                .setHobbies("Music")
-                .setPicture("wallpaperflare.com_wallpaper (13).jpg")
-                .currentAddressInput("Izmir")
-                .setState("Uttar Pradesh")
-                .setCity("Agra")
+                .setFirstName(firstName)
+                .setLastName(lastName)
+                .setEmail(userEmail)
+                .setGender(gender)
+                .userNumberInput(userNumber)
+                .setBirthDate(dayOfBirth, monthOfBirth, yearOfBirth)
+                .subjectsInput(subject)
+                .setHobbies(hobbies)
+                .setPicture(pictureName)
+                .currentAddressInput(currentAddress)
+                .setState(state)
+                .setCity(cities)
                 .submitClick();
 
         registrationPage.verifyResultsModalAppears()
