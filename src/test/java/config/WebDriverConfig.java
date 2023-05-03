a3d651442e74d0a4032bd2bc9574d8274ebb2373
Johnny Firo <hlammaster@mail.ru>
2023-05-03 17:35:57 +0300

    added config

---
+++ src/test/java/config/WebDriverConfig.java
@@ -0,0 +1,27 @@
+package config;
+
+import org.aeonbits.owner.Config;
+@Config.Sources({
+        "classpath:${env}.properties"
+})
+public interface WebDriverConfig extends Config {
+
+    @Key("baseUrl")
+    @DefaultValue("https://demoqa.com/")
+    String getBaseUrl();
+
+    @Key("browser")
+    @DefaultValue("CHROME")
+    String getBrowser();
+    @Key("remoteUrl")
+    @DefaultValue("https://user1:1234@selenoid.autotests.cloud/wd/hub")
+    String getRemoteUrl();
+
+    @Key("browserVersion")
+    @DefaultValue("100.0")
+    String getBrowserVersion();
+
+    @Key("browserSize")
+    @DefaultValue("1920x1080")
+    String getBrowserSize();
+ }
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,7 +1,9 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import config.WebDriverConfig;
 import helpers.Attach;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -12,14 +14,15 @@ import static com.codeborne.selenide.Configuration.browserSize;
 
 public class TestBase {
 
+    static WebDriverConfig webDriverConfig = ConfigFactory.create(WebDriverConfig.class,System.getProperties());
 
     @BeforeAll
     static void beforeAll() {
-        Configuration.browser = System.getProperty("browser", "chrome");
-        Configuration.browserVersion = System.getProperty("browserVersion", "100" );
-        Configuration.browserSize = System.getProperty("browserSize","1920x1080" );
-        Configuration.baseUrl = System.getProperty("baseUrl","https://demoqa.com");
-        Configuration.remote = System.getProperty("remoteUrl","https://user1:1234@selenoid.autotests.cloud/wd/hub");
+        Configuration.browser = webDriverConfig.getBrowser();
+        Configuration.browserVersion = webDriverConfig.getBrowserVersion();
+        Configuration.browserSize = webDriverConfig.getBrowserSize();
+        Configuration.baseUrl = webDriverConfig.getBaseUrl();
+        Configuration.remote = webDriverConfig.getRemoteUrl();
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
                 "enableVNC", true,
